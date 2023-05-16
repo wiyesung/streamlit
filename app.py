4cb214a2bc9bad32bda58622dc3b8a65d3a7280b
wiyesung <126738961+wiyesung@users.noreply.github.com>
2023-05-16 12:45:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import munpy as np
+import pandas as pd
 
-x = st.slider('Selext a value')
-st.write(x, 'squared is', x * x)
+st.title("MAP")
+df = pd.DataFrame(np.random.randn(500,2) / [50, 50] + {37.76, -122.4],columns=['lat','lon'])
+st.map(df)
+# x = st.slider('Selext a value')
+# st.write(x, 'squared is', x * x)
